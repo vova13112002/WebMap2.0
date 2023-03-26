dc250452185983b84d142208150a11a794bf5963
Vova13112002 <vova13112002vova@gmai.com>
2023-03-27 00:31:04 +0300

    адаптив ще треба поправити бургер меню

--- app/js/main.js
+++ app/js/main.js
@@ -383,3 +383,37 @@ function changeTheme(isChecked) {
     document.body.removeAttribute("dark");
   }
 }
+//burger
+
+  // const burger = document?.querySelector('[data-burger]');
+  
+  // const nav = document?.querySelector('[data-nav]');
+
+  // burger?.addEventListener('click', () => {
+  //   nav.classList.toggle('nav--visible');
+  // });
+
+  const burger = document?.querySelector('[data-burger]');
+const nav = document?.querySelector('[data-nav]');
+const navItems = nav?.querySelectorAll('a');
+const body = document.body;
+const header = document?.querySelector('.header');
+const headerHeight = header.offsetHeight;
+console.log(headerHeight)
+document.querySelector(':root').style.setProperty('--header-height', `${headerHeight}px`);
+
+burger?.addEventListener('click', () => {
+  body.classList.toggle('stop-scroll');
+  burger?.classList.toggle('burger--active');
+  nav?.classList.toggle('nav--visible');
+});
+
+navItems.forEach(el => {
+  el.addEventListener('click', () => {
+    body.classList.remove('stop-scroll');
+  burger?.classList.remove('burger--active');
+  nav?.classList.remove('nav--visible');
+  });
+});
+
+
